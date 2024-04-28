0ae7a9a4e6e0e74730ee8134d1984af07da0d702
Frank Borja <fborjaz@unemi.edu.ec>
2024-04-28 13:11:08 -0500

    frist add

---
+++ calculos.py
@@ -0,0 +1,12 @@
+# calculos.py
+
+from abc import ABC, abstractmethod
+
+class Icalculo(ABC):
+    @abstractmethod
+    def cal_iva(self,iva=0.12,valor=0):
+        pass
+
+    @abstractmethod
+    def cal_discount(self,valor=0,discount=0):
+        pass
